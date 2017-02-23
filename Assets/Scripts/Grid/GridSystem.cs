d0b2680c08b8b5c9a6f84899c01f3aa3bd9ee87a
Terry Lim <lnxterry@gmail.com>
2017-02-23 22:53:31 +0800

    Hopefully this unfucks the fuckery.

--- Assets/Scripts/Grid/GridSystem.cs
+++ Assets/Scripts/Grid/GridSystem.cs
@@ -402,17 +402,23 @@ public class GridSystem : MonoBehaviour
 		}
 	}
 
-	public void SaveToCSV(string _folderPath) {
+	public void SaveToCSV(string _folderPath)
+	{
 		//If there's a grid, save as 1. Else, save as 0.
 		string[] gridData = new string[numRows]; //Add numRows to account for the extra "/r" at the end.
-		for (int row = 0; row < numRows; ++row) {			
+		for (int row = 0; row < numRows; ++row)
+		{			
 			gridData[row] = "";
-			for (int column = 0; column < numColumns; ++column) {				
+			for (int column = 0; column < numColumns; ++column)
+			{
 				GridID gridID = ComputeID(row, column);
-				if (GetGrid(gridID) == null) {
+				if (GetGrid(gridID) == null)
+				{
 					//string.Concat(gridData[row], "0,");
 					gridData[row] += "0,";
-				} else {
+				}
+				else
+				{
 					//string.Concat(gridData[row], "1,");
 					gridData[row] += "1,";
 				}
@@ -422,14 +428,16 @@ public class GridSystem : MonoBehaviour
 		string fileName = "New Grid Layout";
 		string filePath = _folderPath + "\\" + fileName; //The file path.
 		int numTries = 1; //Start from index 1 instead of zero because that seems to be how most files are done.
-		while (File.Exists(filePath + ".csv")) {
+		while (File.Exists(filePath + ".csv"))
+		{
 			filePath = _folderPath + "\\" + fileName + " (" + numTries.ToString() + ")";
 			++numTries;
 		}
 		filePath += ".csv";
 
 		var file = File.CreateText(filePath);
-		for (int row = 0; row < numRows; ++row) {
+		for (int row = 0; row < numRows; ++row)
+		{
 			//print(gridData[row]);
 			file.WriteLine(gridData[row]);
 		}
@@ -438,24 +446,47 @@ public class GridSystem : MonoBehaviour
 		print("Saving Grid Layout to " + filePath + ". Refresh folder for it to show up. If it does not appear after 30 seconds, tell Terry. Remember to Calculate Neighbours if you haven't done so.");
 	}
 
-	public void RenderGrids(bool _render = true) {
-		for (int i = 0; i < grids.Length; ++i) {
-			if (grids[i] != null) {
+	public void RenderGrids(bool _render = true)
+	{
+		for (int i = 0; i < grids.Length; ++i)
+		{
+			if (grids[i] != null)
+			{
 				grids[i].GetComponent<MeshRenderer>().enabled = _render;
 			}
 		}
 	}
 
-	public int GetNumRows() {
+	public int GetNumRows()
+	{
 		return numRows;
 	}
 
-	public int GetNumColumns() {
+	public int GetNumColumns()
+	{
 		return numColumns;
 	}
 
-	public List<GridID> GetTritanCrystalGridIDs() {
+	public List<GridID> GetTritanCrystalGridIDs()
+	{
 		return tritanCrystalGridIDs;
 	}
+
+    public void RemoveGridsInsideTerrain(Terrain _terrain)
+	{
+        for (int i = 0; i < grids.Length; ++i)
+		{
+			if (grids[i] == null) 
+			{
+				continue;
+			}
+			if (_terrain.SampleHeight(grids[i].transform.position) > grids[i].transform.position.y)
+			{
+				GameObject.DestroyImmediate(grids[i]);
+            }
+        }
+
+		SetGridsNeighbours();
+    }
 
 }
